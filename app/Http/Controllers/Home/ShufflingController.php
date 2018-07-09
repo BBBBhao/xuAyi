0e926c6fcb8d9aa4927786486c2ca6e59bca681b
bbbbhao <15130543568@163.com>
2018-07-09 11:29:04 +0800

    bh

--- app/Http/Controllers/Home/ShufflingController.php
+++ app/Http/Controllers/Home/ShufflingController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class AbController extends Controller
+class ShufflingController extends Controller
 {
     /**
      * Display a listing of the resource.
