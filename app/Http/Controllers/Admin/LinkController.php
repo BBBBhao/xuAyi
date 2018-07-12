7f3c28f9a8f8f1e33b13154c742a84a5438d9da6
Jinxinaze <fxmdd520jxz@163.com>
2018-07-12 19:52:24 +0800

    jxz

--- app/Http/Controllers/Admin/LinkController.php
+++ app/Http/Controllers/Admin/LinkController.php
@@ -26,7 +26,7 @@ class linkController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
+     ******
      * @return \Illuminate\Http\Response
      */
     public function create()
